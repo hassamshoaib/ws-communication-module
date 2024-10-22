9b0802d7fa750a0841866c670005d88d4569579e
hassamshoaib <hassamshoaib96@gmail.com>
2024-10-22 11:15:48 +0500

    Add websocket actions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import WebSocketModule from "./websocket-module/WebSocketModule";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [messageInput, setMessageInput] = useState("");
+  const [socketModule1, setSocketModule1] = useState(null);
+  const webSocketUrl = "ws://localhost:8080"; // Replace with your WebSocket server URL
+
+  useEffect(() => {
+    setSocketModule1(
+      new WebSocketModule(webSocketUrl, (payload) => {
+        handleWebSocketMessage(payload);
+      })
+    );
+    // Optional: you can also store wsModule to send messages later if needed
+    return () => {
+      // Cleanup or disconnect WebSocket if needed
+    };
+  }, []);
+
+  const handleWebSocketMessage = (payload) => {
+    setMessages((prevMessages) => [...prevMessages, payload.message]);
+  };
+
+  const handleSendMessage = () => {
+    if (messageInput.trim() !== "") {
+      socketModule1.sendMessage({
+        type: "SEND_WEBSOCKET_MESSAGE",
+        payload: { message: messageInput }
+      });
+
+      setMessageInput("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>WebSocket Communication</h1>
+      <div>
+        <input
+          type="text"
+          value={messageInput}
+          onChange={(e) => setMessageInput(e.target.value)}
+        />
+        <button onClick={handleSendMessage}>Send Message</button>
+      </div>
+      <div>
+        <h2>Messages:</h2>
+        <ul>
+          {messages.map((msg, index) => (
+            <li key={index}>{msg}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
